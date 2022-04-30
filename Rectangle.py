58467ec3d39fca8d5311167d97f452311c0d974c
Jiarong Jin <98533787+jjin16@users.noreply.github.com>
2022-04-30 01:22:03 +0000

    final push

--- Rectangle.py
+++ Rectangle.py
@@ -1,5 +1,10 @@
 class Rectangle:
   def __init__(self,x,y,height,width):
+    """
+    Sets up the instance variable for rectangle object
+    Args:(integer)x, (integer)y, (integer)height, (integer)width
+    return: none
+    """
     if x<0:
       x=0
     if y<0:
@@ -13,5 +18,10 @@ class Rectangle:
     self.height=height
     self.width=width
   def __str__(self):
+    """
+    Stores the parameters of rectangle object into info_string and returns it.
+    Args: none
+    Return:(string) the values for rectangle parameters
+    """
     info_string=f"(x : {self.x}, y: {self.y}) width: {self.width}, height: {self.height}"
     return info_string
--- Surface.py
+++ Surface.py
@@ -1,7 +1,17 @@
 from Rectangle import Rectangle
 class Surface:
   def __init__(self, filename, x, y, height, width):
+    """
+    Sets up the instance variable for rectangle object
+    Args:(string)filename, (integer)x, (integer)y, (integer)height, (integer)width
+    return: none
+    """
     self.rect=Rectangle(x,y,height,width)
     self.image=str(filename)
   def getRect(self):
+    """
+    returns the filename parameter of the surface object 
+    Args: none
+    return:(string) The filename parameter representing the file path
+    """
     return self.rect
